eda0f2ffa569cd7c630bf212b89c157876176fcd
Josué B <jbrenesr1@ucenfotec.ac.cr>
2021-10-21 21:34:06 -0600

    Feature/create team (#8)
    * Varios
    
    * Crear Equipo
    
    Se crea un form para guardar el equipo
    
    * Fix
    
    Se hace Merge con el trabajo de Sebastian
    
    * Fix Main
    
    Se eliminan entradas duplicadas
    
    * Feature done
    
    Funcionalidad está funcionando al 100%

--- app/src/main/java/com/teapot/temtempedia/SaveTeam.kt
+++ app/src/main/java/com/teapot/temtempedia/SaveTeam.kt
@@ -1,51 +1,63 @@
 package com.teapot.temtempedia
 
 import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.google.common.reflect.TypeToken
+import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.Gson
+import com.teapot.temtempedia.models.Team
 import com.teapot.temtempedia.models.Temtem
+import com.teapot.temtempedia.models.addTeam
+import org.json.JSONArray
 import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
+class SaveTeam : AppCompatActivity()  {
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private var team = TeamLocal()
+    private lateinit var editTextHello: EditText
     private val fragmentManager = supportFragmentManager
+    private var temtemList : MutableList<Int?> = mutableListOf()
+
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_save_team)
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.web_client_id))
-            .requestEmail()
-            .build()
-        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+        editTextHello = findViewById(R.id.editTextTextPersonName)
 
 
-        findViewById<Button>(R.id.logout).setOnClickListener {
-            mGoogleSignInClient.signOut().addOnCompleteListener {
-                val intent= Intent(this, LoginScreen::class.java)
-                Toast.makeText(this,"Logging Out",Toast.LENGTH_SHORT).show()
-                startActivity(intent)
-                finish()
+        findViewById<Button>(R.id.btnSave).setOnClickListener {
+            team.nombre = editTextHello.text.toString()
+
+            addTeam(
+                Team(
+                    userId = auth.uid,
+                    temtemIds = temtemList,
+                    title = team.nombre
+                )
+            ).addOnSuccessListener { ref ->
+                run {
+                    Snackbar.make(it, "Team saved ${ref.path}", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+                }
             }
         }
 
-        findViewById<Button>(R.id.viewTeams).setOnClickListener {
-            val intent = Intent(this, TeamActivity::class.java)
-            startActivity(intent)
-        }
-
         printTemtemList()
+
     }
+
 
     private fun getJsonDataFromAsset(context: Context, fileName: String): String? {
         val jsonString: String
@@ -66,11 +78,15 @@ class MainActivity : AppCompatActivity() {
         val listaTemtem = object : TypeToken<List<Temtem>>() {}.type
         var temtem: List<Temtem> = gson.fromJson(jsonFileString, listaTemtem)
 
+
+
+
+
         temtem.forEachIndexed { _, temp ->
             var id: Int = temp.id
             var name: String = temp.nombre
             var thumb: String = temp.fotoNormal
-            var show  = false;
+            var show  = true;
 
             var temtemFragmentInstance = TemtemListItemMainActivity.newInstance(id, name, thumb, show)
             var transaction = fragmentManager.beginTransaction()
@@ -78,7 +94,18 @@ class MainActivity : AppCompatActivity() {
             transaction.commit()
 
         }
+
+
+
+    }
+
+     fun passDataCom(data: MutableList<Int?>) {
+         temtemList = data
     }
 
 
 }
+
+
+
+
--- app/src/main/java/com/teapot/temtempedia/TeamActivity.kt
+++ app/src/main/java/com/teapot/temtempedia/TeamActivity.kt
@@ -1,18 +1,16 @@
 package com.teapot.temtempedia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 import com.teapot.temtempedia.adapters.TeamAdapter
 import com.teapot.temtempedia.models.Team
-import com.teapot.temtempedia.models.addTeam
 
 class TeamActivity : AppCompatActivity() {
     private val db by lazy { FirebaseFirestore.getInstance() }
@@ -25,19 +23,17 @@ class TeamActivity : AppCompatActivity() {
         setContentView(R.layout.activity_team)
         setUpRecyclerView()
         findViewById<FloatingActionButton>(R.id.fabAdd).setOnClickListener {
-            addTeam(Team(userId = auth.uid, temtemIds = listOf(59,12,9), title = "${auth.currentUser?.displayName}'s Team")).addOnSuccessListener { ref ->
-                run {
-                    Snackbar.make(it, "Team saved ${ref.path}", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                }
-            }
-            adapter.notifyDataSetChanged()
+
+            val intent = Intent(this, SaveTeam::class.java)
+            startActivity(intent)
         }
+
     }
 
     private fun setUpRecyclerView() {
         val query = teamCollection.whereEqualTo("userId", auth.uid)
-        val opts = FirestoreRecyclerOptions.Builder<Team>().setQuery(query, Team::class.java).build()
+        val opts =
+            FirestoreRecyclerOptions.Builder<Team>().setQuery(query, Team::class.java).build()
         adapter = TeamAdapter(opts, applicationContext)
         val rvTeam = findViewById<RecyclerView>(R.id.rvTeamDisplay)
         rvTeam.setHasFixedSize(false)
